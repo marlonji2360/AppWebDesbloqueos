7cd653a8e85de99e36cd5d498436d8e0d3f0ad72
Marlon I. Jimenez (Analista Programador Core) <marlon_jimenez@bantrab.net.gt>
2024-09-05 17:12:36 -0600

    Version CRUD completa

--- AppWebDesbloqueos/obj/Debug/net8.0/AppWebDesbloqueos.AssemblyInfo.cs
+++ AppWebDesbloqueos/obj/Debug/net8.0/AppWebDesbloqueos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppWebDesbloqueos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862ef66a553d7f1132c5280630fd4ff452ba8b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e54ca7bdc241feaa61efadabab61ba09ba3cc55")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppWebDesbloqueos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppWebDesbloqueos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
